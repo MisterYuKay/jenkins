eb162f8828a101be1cc710387d62520ee0bd5334
MisterYuKay <57484482+MisterYuKay@users.noreply.github.com>
2020-03-14 18:05:43 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,10 +1,6 @@
-public class AddTwoNumbers {
+public class Add{
 
    public static void main(String[] args) {
-        
-      int num1 = 5, num2 = 15, sum;
-      sum = num1 + num2;
-
-      System.out.println("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers is 42");
    }
 }
